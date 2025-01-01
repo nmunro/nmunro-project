e357d98c9947c22e03b17d7058f5f73c1ba52a20
NMunro <neilmunro@gmail.com>
2025-01-01 14:07:23 +0000

    Fixed a small quoting inconsistency

--- src/skeleton/tests.lisp
+++ src/skeleton/tests.lisp
@@ -4,7 +4,7 @@
         :rove))
 (in-package :{{project-name}}/tests/main)
 
-;; NOTE: To run this test file, execute `(asdf:test-system :{{project-name}})' in your Lisp.
+;; NOTE: To run this test file, execute `(asdf:test-system :{{project-name}})` in your Lisp.
 
 (deftest test-target-1
   (testing "should (= 1 1) to be true"
